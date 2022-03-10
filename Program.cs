650dca1b95e010cefc72fa6d881a470b77e902b7
User <tu7dsvbpa@code.edu.az>
2022-03-10 11:24:02 +0400

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿ using System;
+
+namespace mustafatask2
+{
+    public class program
+    {
+         static void Main(string[] args)
+         {
+            int[] arr = { 12, 45, 5, 25, 36 };
+
+            int maxnumber = arr[0];
+
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                if (arr[i] > maxnumber)
+                {
+                    maxnumber = arr[i];
+                }
+            }
+             Console.WriteLine(maxnumber);
+         }
+
+
+    }
+        
+    
+}
